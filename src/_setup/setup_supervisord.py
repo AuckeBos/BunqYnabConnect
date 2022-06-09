4c556b3f5b3fcf571bf1a9c9a1e704fc39d1924e
Aucke Bos <aucke.bos97@gmail.com>
2022-06-09 20:07:54 +0200

    Fixed setup of supervisord

--- src/_setup/setup_supervisord.py
+++ src/_setup/setup_supervisord.py
@@ -2,25 +2,59 @@ import os
 import subprocess
 
 
+def _supervisor_dir() -> str:
+    """
+    Get the supervisor dir
+    """
+    dir = os.path.dirname(os.path.realpath(__file__))
+    config_dir = f"{dir}/../../supervisor"
+    return config_dir
+
+
+def _user() -> str:
+    """
+    Get the current user
+    """
+    return subprocess.check_output("whoami").strip().decode("utf-8")
+
+
+def _create_conf_file():
+    """
+    Create a config file, based on the config template. The actual file has the <USER>
+    variable replaced with the current user
+    """
+    dir = _supervisor_dir()
+    template_file = f"{dir}/supervisord.conf.template"
+    with open(template_file, "r") as f:
+        template = f.read()
+    updated_content = template.replace("<USER>", _user())
+    conf_file = f"{dir}/supervisord.conf"
+    with open(conf_file, "w") as f:
+        f.write(updated_content)
+
+
 def _add_supervisord_to_cron():
     """
     Add a line to the crontab, which starts supervisord on reboot
     """
     print("Adding supervisor to cron, such that supervisord starts on reboot")
-    dir = os.path.dirname(os.path.realpath(__file__))
+    supervisor_dir = _supervisor_dir()
     supervisord_loc = "$(poetry run which supervisord)"
-    config_dir = f"{dir}/../../supervisor/supervisord.conf"
-    cron_line = f"@reboot {supervisord_loc} -c {config_dir} 2>&1"
-    user = subprocess.check_output("whoami").strip().decode("utf-8")
+    config_file = f"{supervisor_dir}/supervisord.conf"
+    cron_line = f"@reboot {supervisord_loc} -c {config_file} 2>&1"
+    user = _user()
     command = f'(crontab -u {user} -l; echo "{cron_line}") | crontab -u {user} -'
     current_cron = subprocess.check_output(f"crontab -u {user} -l", shell=True).decode(
         "utf-8"
     )
-    # If the current dir exists in the cron file, this command is already added once,
-    # dont't re-add it
-    if config_dir in current_cron:
+    # If the current config_file exists in the cron file, this command is already added
+    # once dont't re-add it
+    if config_file in current_cron:
         print("Not updating cron, command yet exists in crontab")
     else:
+        # First create the actual config file, based on the template
+        _create_conf_file()
+        # Then add the cron task
         os.system(command)
         print("Cron task added")
     print("Your current cron is as follows:")
